8af08e01c6f86c79a900beab73be2410befafa9f
Christian Karlsson <krille.karlsson@gmail.com>
2016-12-19 19:54:06 +0100

    Add project files.

---
+++ test.testprojekt/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test.testprojekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "Hello World!";
+            string name = "Christian Karlsson";
+            string[] words = SplitName(name);
+
+            foreach(var str in words)
+            {
+                Console.WriteLine(str);
+            }
+
+            Console.WriteLine(greeting);
+            Console.ReadLine();
+        }
+
+        static string[] SplitName(string str)
+        {
+            return str.Split();
+        }
+    }
+}
